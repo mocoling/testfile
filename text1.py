f8b0317bdc52ea06deed515618108e918307e8cb
黄雅茹 <huangyaru@mocoling.local>
2023-09-02 15:31:20 +0800

    test over

--- text1.py
+++ text1.py
@@ -6,3 +6,8 @@ you see this lan mind i change the first text.
 
 you see this lan mind i back to first change,and this is a conclusion: if you
 just add nothing change at historical version, you have to commit it.
+
+now testing revoke 
+        1.is that git restore <file> is revoke local file? Yes
+	2.is that git restore --staged <file> is revoke to add file? Yes
+	 
