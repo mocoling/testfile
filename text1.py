ec340886fdb59012e52672bff39ff98a88b6eccb
黄雅茹 <huangyaru@mocoling.local>
2023-09-02 15:09:18 +0800

    modified the content

--- text1.py
+++ text1.py
@@ -2,5 +2,7 @@ This txt use to text git's add,commit,push,,,,
 
 hello git.
 
-
 you see this lan mind i change the first text.
+
+you see this lan mind i back to first change,and this is a conclusion: if you
+just add nothing change at historical version, you have to commit it.
